1b23bfc398e8a1adbea9a9e61aeb444a3fe7b573
Nitin Arya <nitinarya1001@gmail.com>
2026-09-11 12:38:02 +0530

    added a rate limiter

--- dockerfile
+++ dockerfile
@@ -1,20 +1,7 @@
-# Use a lightweight official Node.js image
 FROM node
-
-# Set the working directory inside the container
 WORKDIR /app
-
-# Copy package.json (and package-lock.json if you have one)
 COPY package.json ./
-
-# Install the dependencies inside the container
 RUN npm install
-
-# Copy the server code and the public folder
 COPY . .
-
-# Expose port 3000 to the outside world
 EXPOSE 3000
-
-# The command to start the server
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import express from "express";
 import QRCode from "qrcode";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
 dotenv.config({ quiet: true });
 
 const PORT = process.env.PORT;
 const app = express();
 
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+  limit: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: "draft-7", // Returns standard RateLimit-* headers
+  legacyHeaders: false, // Disables the older X-RateLimit-* headers
+  statusCode: 429, // Explicitly send HTTP 429
+  message: {
+    error: "Too Many Requests",
+    message: "You have exceeded your 100 requests per 15 minutes limit.",
+    retryAfter: "Check retry headers for precise wait time.",
+  },
+});
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(globalLimiter);
 app.use(express.static("public"));
 
 app.get("/", async (_, res) => {
